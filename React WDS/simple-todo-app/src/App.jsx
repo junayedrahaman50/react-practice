8d02e6b6eb93fffe3b2dca8dcc108a17f2dfef1e
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-03-13 00:47:32 +0530

    learning to build todo list project in react

---
+++ React WDS/simple-todo-app/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./styles.css";
+export default function App() {
+  /* we use class to style elements but inside of react class in a reserved keyword so we need to use className in jsx instead of class attribute in html, In label we are also using htmlFor in place of for attribute */
+
+  /* ------------------------------------------------------------------------------------------
+   useState is a hook, it takes a default value ("") in this case. State is required when we need to update something on the screen. setNewItem is a function to update newItem variable state (data), state variable is immutable. changing state re renders the component
+  ------------------------------------------------------------------------------------------
+   */
+  const [newItem, setNewItem] = useState("");
+  /*   // re renders every time
+  // setNewItem("Infinte Loop!"); */
+
+  /*  return text hi
+    return "Hi React"; */
+  return (
+    /* Convert what our final version should look like to jsx, we declared(in jsx) how our UI/App should look like, when adding interactivity to our site, instead manually changing our values we will change the jsx, declaring what we want in the UI the entire thing(component) will re render, In order to build modified variables and add interactivity we need to use state(useState) (change data) */
+    //fragment <></>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item" className="new-item">
+            New Item
+          </label>
+          {/* update our input when it changes, value of our input = newItem variable value, which is empty string ("") default value of useState in this case, for example check by puting "Default" in useState it will be updated, onChange is fired everytime we click a key in React. It takes event object(e) and gets the value of input on getting event(everytime we click a key) and setting that as value of newItem  */}
+          <input
+            value={newItem}
+            /* if we comment below line we can't fill this input as the input value is of newItem variable which is (""), so it's saying whenever I change my input(onChange) get the new value of my input(e.target.value) set that as newItem and rerun my component with updated value, setNewItem function will be called whenever the input data changes the entire component will re render with updated value, if our input value is set to some specific value it will always have that value until we change it, to update that value we need change the state variable by calling setNewItem function. changing state variable rerenders entire component. Updating state variable(newItem) will be updated everywhere it is used in our component(App) */
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      {/*  Can't return more than one element, fix wrap everything inside a div or
+      wrap everything in a fragment(empty tag) */}
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
---
+++ React WDS/simple-todo-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// get root element and render our app inside
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
