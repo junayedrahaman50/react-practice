0b4e03c2bb97dca22144f9d5ad71f1473acbb728
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-03-23 20:19:19 +0530

    Learned Router, Route, Switch, Link from react-router-dom (React Router)

--- React Net Ninja/dojo-blog/src/App.js
+++ React Net Ninja/dojo-blog/src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import create component
+import Create from "./Create";
 
 function App() {
   // const title = "Welcome to the new blog!";
@@ -13,8 +15,11 @@ function App() {
         <div className="content">
           {/* <Home /> */}
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
---
+++ React Net Ninja/dojo-blog/src/Create.js
@@ -0,0 +1,10 @@
+const Create = () => {
+  // This template will be very simple to begin with, Now we will show this component App.js (App Component)
+  return (
+    <div className="create">
+      <h2>Add a new blog:</h2>
+    </div>
+  );
+};
+
+export default Create;
